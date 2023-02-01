f1b2d2b2d95b3803d8b8804247f6da84759b40ab
baba-s <shotababa1206@gmail.com>
2023-02-01 21:08:06 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.SetDebugUnityLoggerEnabledScope
+# Kogane Set Debug Unity Logger Enabled Scope
+
+UnityEngine.Debug.unityLogger.enabled を設定するスコープ
---
+++ Runtime/SetDebugUnityLoggerEnabledScope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kogane
+{
+    public readonly struct SetDebugUnityLoggerEnabledScope : IDisposable
+    {
+        private readonly bool m_oldLogEnabled;
+
+        public SetDebugUnityLoggerEnabledScope( bool logEnabled )
+        {
+            m_oldLogEnabled              = Debug.unityLogger.logEnabled;
+            Debug.unityLogger.logEnabled = logEnabled;
+        }
+
+        public void Dispose()
+        {
+            Debug.unityLogger.logEnabled = m_oldLogEnabled;
+        }
+    }
+}
